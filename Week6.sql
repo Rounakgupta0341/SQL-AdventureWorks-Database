8fcadd89e186a4545432c67c1af14f865b662a6f
Rounak Gupta <97901356+Rounakgupta0341@users.noreply.github.com>
2024-07-07 13:31:46 +0530

    Create Week6.sql

---
+++ Week6.sql
@@ -0,0 +1,19 @@
+--Question 182. Duplicate Emails
+
+https://leetcode.com/problems/duplicate-emails/submissions/1305028231/
+
+--Question 178. Rank Scores
+
+https://leetcode.com/problems/rank-scores/submissions/1305027453/
+
+--Question 177. Nth Highest Salary
+
+https://leetcode.com/problems/nth-highest-salary/submissions/1305025450/
+
+--Question 176. Second Highest Salary
+
+https://leetcode.com/problems/second-highest-salary/submissions/1305021316/
+
+--Question 175. Combine Two Tables
+
+https://leetcode.com/problems/combine-two-tables/submissions/1305020226/
